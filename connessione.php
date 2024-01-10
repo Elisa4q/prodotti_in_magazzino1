8a46f41792d7bcb3675aa89148e2680a0fa4d724
Elisa4q <145005271+Elisa4q@users.noreply.github.com>
2024-01-10 15:34:31 +0100

    Update connessione.php

--- connessione.php
+++ connessione.php
@@ -3,22 +3,26 @@
     <head></head>
     <body>
 
-<p style : "center"><strong>Connnessione al database</strong><br><br><br>
+<html>
+    <head></head>
+    <body>
+<p aligin = "center"><strong>Connnessione al database</strong><br><br><br>
 <?php
 $hostname = "localhost";
 $username = "root";
 $password  = "";
 $dbname = "prodotti_in_magazzino1";
 
-$conn = mysqli_connect($hostname, $username, $password, $dbname);
+$conn = mysqli _connect($hostname, $username, $password, $dbname);
 if(!$conn)
     die("<br><br><strong>Errore nella connessione</strong>");
 else
-    print "<br><br><strong>Connessione avvenuta correttamente</strong><br><br>";
+    print "<br><br> <strong> Connessione avvenuta correttamente</strong><br><br>";
 
-mysqli_close($conn);
-print "<strong>Ora la connessione è chiusa</strong>";
-
+mysqli_close($conn)
+print "<strong>Ora la connessione è chiusa</strong>"
 ?>
 </body>
 </html>
+
+
