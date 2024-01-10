287f4fc792784864ca15459d301fb18eae54e484
Elisa4q <145005271+Elisa4q@users.noreply.github.com>
2024-01-10 15:23:47 +0100

    Create elenco_prodotti.php

---
+++ elenco_prodotti.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang = "it">
+    <head></head>
+    <body>
+      <p style : "center"><strong>Connnessione al database</strong><br><br><br>
+<?php
+$hostname = "localhost";
+$username = "root";
+$password  = "";
+$dbname = "prodotti_in_magazzino1";
+
+
+$conn = mysqli_connect($hostname, $username, $password, $dbname);
+if(!$conn){
+    die("<br><br><strong>Errore nella connessione</strong>");
+    exit();
+}
+$query = "select * from prodotti order by prezzo_unitario";
+$risultato = mysqli_query($conn, $query);
+
+if($risultato){
+    print"errore nel comando";
+    exit();
+}
+
+
+$riga = mysqli_fetch_array($risultato);
+if($riga){
+    print "<table border = '1'>";
+    print"<tr><td> CODICE </td><td> DESCRIZIONE </td><td> PREZZO </td><td> QUANTITA</td></tr>";
+
+    while($riga){
+        print "<tr><td>" .$riga['codice_prodotto']. "</td>";
+        print "<td>" .$riga['descrizione']. "</td>";
+        print "<td>" .$riga['prezzo_unitario']. "</td>";
+        print "<td>" .$riga['quantita']. "</td></tr>";
+        $riga = mysqli_fetch_array($risultato);
+    }
+    print "</table>";
+} else {
+    print "Attenzione non ci prodotti presenti";
+}
+mysqli_close($conn);
+?>
+</body>
+</hmtl>
+ 
+
+
+
+?>
+</body>
+</html>
