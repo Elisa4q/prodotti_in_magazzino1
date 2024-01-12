dc519b242609241dab2b6b52683df510d1fbc2a6
Elisa4q <elisa.santomauro@iismajoranamoncalieri.edu.it>
2024-01-12 10:45:52 +0100

    up_to_date

--- elenco_prodotti.php
+++ elenco_prodotti.php
@@ -2,7 +2,7 @@
 <html lang = "it">
     <head></head>
     <body>
-      <p style : "center"><strong>Connnessione al database</strong><br><br><br>
+      <p style : "center"><strong>Lista prodotti</strong><br><br><br>
 <?php
 $hostname = "localhost";
 $username = "root";
@@ -15,15 +15,15 @@ if(!$conn){
     die("<br><br><strong>Errore nella connessione</strong>");
     exit();
 }
-$query = "select * from prodotti order by prezzo_unitario";
+$query = "select * from prodotti order by prezzzo_unitario";
 $risultato = mysqli_query($conn, $query);
 
-if($risultato){
-    print"errore nel comando";
+if(!$risultato){
+    print"Errore nell'esecuzione della query SQL";
     exit();
 }
 
-$riga = mysqli_fetch_array($risultato);
+$riga=mysqli_fetch_array($risultato);
 if($riga){
     print "<table border = '1'>";
     print"<tr><td> CODICE </td><td> DESCRIZIONE </td><td> PREZZO </td><td> QUANTITA</td></tr>";
@@ -31,9 +31,9 @@ if($riga){
     while($riga){
         print "<tr><td>".$riga['codice_prodotto']. "</td>";
         print "<td>".$riga['descrizione']. "</td>";
-        print "<td>".$riga['prezzo_unitario']. "</td>";
-        print "<td>".$riga['quantita']. "</td></tr>";
-        $riga = mysqli_fetch_array($risultato);
+        print "<td>".$riga['prezzzo_unitario']. "</td>";
+        print "<td>".$riga['quantità']. "</td></tr>";
+        $riga=mysqli_fetch_array($risultato);
     }
     print "</table>";
 } else{
