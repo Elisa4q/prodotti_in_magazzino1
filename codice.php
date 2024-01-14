19af1583a759f2f67d13f5786b526597a7447156
Elisa4q <145005271+Elisa4q@users.noreply.github.com>
2024-01-14 18:06:19 +0100

    Update and rename codice_prodotto.hmtl to codice.php

---
+++ codice.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+<head></head>
+<body> 
+  <?php
+  $hostname = "localhost";
+  $username = "root";
+  $password= "";
+  $dbname = "prodotti_in_magazzino1";
+
+//connessione al server sql
+$conn = mysqli_connected($hostname, $username, password, dname);
+  if(!$conn){
+    die("ERRORE NELLA CONNESSIONE");
+    exit();
+  }
+//recupera i dati dal form html
+
+$codice = $_POST["codice"];
+$query = "select * from prodotti where codice_prodotto = $codice_prodotto";
+$risultato = mysqli_query($conn, $query);
+  if(!$risultato)
+  {
+    print "ERRORE NEL COMANDO";
+    exit();
+  }
+    $riga = mysqli_fetch_array($risultato);
+    if($riga)
+    {
+        print "descrizione: ".$riga['descrizione'].<br>";
+        print "prezzo_unitario: ".$riga['prezzo_unitario'].<br>";
+        print  "quantita: ".$riga['quantita"'].<br>";
+      }
+          else{
+            print "ATTENZIONE CODICE PRODOTTO : $codice_prodotto NON E' PRESENTE ";
+          }
+        mysqli_close($conn);
+?>
+</body>
+</hmtl>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
