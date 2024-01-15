c3fab3d89e092a0ea77fd584af081fd014b52fe2
Elisa4q <elisa.santomauro@iismajoranamoncalieri.edu.it>
2024-01-15 10:39:33 +0100

    modifiche

--- codice.php
+++ codice.php
@@ -9,14 +9,14 @@
   $dbname = "prodotti_in_magazzino1";
 
 //connessione al server sql
-$conn = mysqli_connected($hostname, $username, password, dname);
+$conn = mysqli_connect($hostname, $username, $password, $dbname);
   if(!$conn){
     die("ERRORE NELLA CONNESSIONE");
     exit();
   }
 //recupera i dati dal form html
 
-$codice = $_POST["codice"];
+$codice_prodotto = $_POST["codice_prodotto"];
 $query = "select * from prodotti where codice_prodotto = $codice_prodotto";
 $risultato = mysqli_query($conn, $query);
   if(!$risultato)
@@ -27,9 +27,9 @@ $risultato = mysqli_query($conn, $query);
     $riga = mysqli_fetch_array($risultato);
     if($riga)
     {
-        print "descrizione: ".$riga['descrizione'].<br>";
-        print "prezzo_unitario: ".$riga['prezzo_unitario'].<br>";
-        print  "quantita: ".$riga['quantita"'].<br>";
+        print "descrizione: ".$riga['descrizione']. "<br>";
+        print "prezzo_unitario: ".$riga['prezzo_unitario']."<br>";
+        print  "quantita: ".$riga['quantita']. "<br>";
       }
           else{
             print "ATTENZIONE CODICE PRODOTTO : $codice_prodotto NON E' PRESENTE ";
