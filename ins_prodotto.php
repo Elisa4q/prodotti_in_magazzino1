9ad735afe7d14764e54386daead1f041ff8ae6e9
Elisa4q <elisa.santomauro@iismajoranamoncalieri.edu.it>
2024-01-17 11:34:13 +0100

    aggiunto

---
+++ ins_prodotto.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang = "it">
+    <head>
+        <meta charset = "utf-8">
+        <title>INSERIMENTO NUOVO PRODOTTO</title>
+    </head>
+    <body>
+        <p>Inserisci i tuoi dati del prodotto:</p>
+        <form method = "post" action = "form.html">
+            codice_prodotto: <input type = "text" value = "inserisci il codice del prodotto" name = "codice_prodotto"> <br><br>
+            descrizione: <input type = "text"  value = "inserisci la descrizione del prodotto" name = "descrizione"> <br><br>
+            prezzo_unitario: <input type = "decimal"  value = "inserisci il prezzo unitario" name = "prezzo_unitario"> <br><br>
+            quantita: <input type = "number"  value = "inserisci la quantita dle prodotto" name = "quantita"> <br><br>
+            <br><br>
+
+            <input type = "submit" value = "Inserisci"> &nbsp; &nbsp; &nbsp;
+            <input type = "reset" value = "Annulla">
+
+        </form>
+    </body>
+</html>
