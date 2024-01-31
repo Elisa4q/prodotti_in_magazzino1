c0f66a93395a0545c657a6e013954f46f2c2b55d
Elisa4q <elisa.santomauro@iismajoranamoncalieri.edu.it>
2024-01-31 12:40:37 +0100

    modifiche

---
+++ codice_prodotto.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang = "it">
+<html>
+<head></head>
+<body>
+  <p><font size = "center"><strong>Home</strong></font><br><br><br>
+  <form method="post" action = "codice.php" size = "10">
+  codice_prodotto: <input type = "text" name = "codice_prodotto">
+  
+
+  <p>&nbsp</p>
+  </p>
+    <input type = "submit" value = "visualizza"> &nbsp; &nbsp; &nbsp;
+    <input type = "reset" value = "Annulla">
+  </form>
+  <form method="post" action = "accedi.php" size = "10">
+  <form method="post" action = "registrazione.php" size = "10">
+</body>
+</html>
