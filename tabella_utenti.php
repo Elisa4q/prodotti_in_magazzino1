ff1f8bf452a9a7fc8456d535ba8f4b9def92ab2e
Elisa4q <elisa.santomauro@iismajoranamoncalieri.edu.it>
2024-02-05 11:06:13 +0100

    modifiche

---
+++ tabella_utenti.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang = "it">
+    <head></head>
+    <body>
+      <p style : "center"><strong>Lista utenti</strong><br><br><br>
+<?php
+$hostname = "localhost";
+$username = "root";
+$password  = "";
+$dbname = "prodotti_in_magazzino1";
+
+
+$conn = mysqli_connect($hostname, $username, $password, $dbname);
+if(!$conn){
+    die("<br><br><strong>Errore nella connessione</strong>");
+    exit();
+}
+$query = "select * from utenti order by Nome";
+$risultato = mysqli_query($conn, $query);
+
+if(!$risultato){
+    print"Errore nell'esecuzione della query SQL";
+    exit();
+}
+
+$riga=mysqli_fetch_array($risultato);
+if($riga){
+    print "<table border = '1'>";
+    print"<tr><td> Nome </td><td> Cognome </td><td> Username </td><td> Password</td></tr>";
+
+    while($riga){
+        print "<tr><td>".$riga['Nome']. "</td>";
+        print "<td>".$riga['Cognome']. "</td>";
+        print "<td>".$riga['Username']. "</td>";
+        print "<td>".$riga['Password']. "</td></tr>";
+        $riga=mysqli_fetch_array($risultato);
+    }
+    print "</table>";
+} else{
+    print "Attenzione utente non trovato";
+}
+mysqli_close($conn);
+?>
+</body>
+</hmtl>
+ 
