0e2ff8de00d04ca690717bb52f39904ddc15b526
Elisa4q <elisa.santomauro@iismajoranamoncalieri.edu.it>
2024-02-05 10:27:11 +0100

    modifiche

--- accedi.php
+++ accedi.php
@@ -17,6 +17,20 @@ if(!$conn){
 }
 
 
+print"<form method='post' action = 'login.php'>";
+print"Username: <input type = 'text' name = 'Username'><br><br>";
+print"Password: <input type = 'text' name = 'Password'><br><br>";
+print"<input type = 'submit' value = 'Accedi'> &nbsp; &nbsp; &nbsp;";
+print"<input type = 'reset' value = 'Reset'>";
+print"</form>";
+
+
+?>
+</body>
+</hmtl>
+ 
+
+
  
 
 
